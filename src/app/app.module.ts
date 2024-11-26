4a655584409fddf8b8c655d7e5ad7ecdd6bef8cb
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2024-11-25 22:04:56 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
