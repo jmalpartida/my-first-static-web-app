fd0475a6b099c532636fbeb6359d648784b0d013
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2024-11-25 21:36:11 -0500

    Update mi-componente.component.ts

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -1,1 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente',
+  templateUrl: './mi-componente.component.html',
+  styleUrls: ['./mi-componente.component.css']
+})
+export class MiComponenteComponent {
+  titulo: string = '¡Hola desde Mi Componente!';
+}
 
