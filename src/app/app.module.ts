b2e4fd01ce6df92f07ae1aead2b6c1f0fee1cb41
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2024-11-30 19:33:50 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiComponenteComponent } from './mi-componente.component';
+import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
