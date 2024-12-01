c65d74b53a19ad36bdf1eb14f378908e2d1247bd
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2024-11-30 20:05:41 -0500

    Update mi-componente.component.ts

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./mi-componente.component.css']
 })
 export class MiComponenteComponent {
-  titulo: string = '¡Hola desde Mi Componente!';
+  titulo: string = '¡Hola desde Mi Componente - Bienvenido a Qhatuyki!';
+  description: string = 'Qhatuyki es la herramienta de productividad en la nube diseñada para los comerciantes de los mercados de abastos en Santiago de Surco, Perú. Con Qhatuyki, podrás vender tus productos en línea, gestionar pedidos, ofrecer descuentos exclusivos y facilitar el pago de forma segura a través de plataformas como Yape y Plin.';
+  features: string[] = [
+    'Crea tu tienda en línea fácilmente.',
+    'Ofrece descuentos y promociones especiales.',
+    'Permite que tus clientes realicen pedidos en línea.',
+    'Acepta pagos a través de Yape, Plin y otras plataformas.',
+    'Visualiza y gestiona los pedidos de tus clientes de forma eficiente.'
+  ];
 }
 
