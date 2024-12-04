dd4a02ea4169e0c96352872ebd613d88437cd8c2
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2024-12-04 18:24:56 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent, MiComponenteComponent
+    AppComponent, 
+    MiComponenteComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
